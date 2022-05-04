b58c2e3bae38d0c2077cdf83bd2e6262b8bff070
Sanjeth Suthahar <sanjeth.suthahar@student.uib.no>
2022-05-04 22:37:08 +0200

    finished for today

--- main.hs
+++ main.hs
@@ -9,6 +9,16 @@ main :: IO ()
 main = do
     putStrLn "\nBoard Positions"
     putStr $ concat $ renderBoardPositions $ fromRenderNotation defaultShownBoard
-    putStrLn "\nRendered Board (with render-note-ids (lower case is 'white' uppercase is 'black'))"
+    putStrLn "\nRendered Board (lower case is 'white' uppercase is 'black')"
     putStr $ concat $ renderBoard $ fromRenderNotation defaultShownBoard
+    putStr "\nWhite to move: "
+    white <- getLine
+    case white of
+        _ -> do
+            putStrLn "Invalid! Try a legal move..."
+    putStr "\nBlack to move"
+    black <- getLine
+    case black of
+        _ -> do
+            putStrLn "Invalid! Try a legal move..."
     return ()
